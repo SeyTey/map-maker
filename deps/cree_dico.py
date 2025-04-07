4119c66c6aeaebe9f4896840781a8959e5ce8c75
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2025-04-07 14:36:13 +0200

    cree dico

---
+++ deps/cree_dico.py
@@ -0,0 +1,18 @@
+import os
+
+def cree_dico(path:str) -> dict:
+    """
+    Les clefs sont les noms des fichiers sans l'extension et les valeurs sont les chemins vers les fichiers.
+    
+    Args:
+        path (str): le chemin vers le dossier contenant les tuiles
+    Returns:
+        dict: les clefs sont les noms des tuiles et les valeurs sont les images
+    """
+    tuiles = os.listdir(path)
+    return {
+        tuile[:-3]:path+'/'+tuile 
+        for tuile in tuiles
+    }
+
+print(cree_dico('tuiles'))
